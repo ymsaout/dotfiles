c178efa2aec869939f02685dbf976cacf20ab129
Yves-Marie Saout <ymsaout42@gmail.com>
2023-03-03 12:11:52 +0100

    init commit

---
+++ bin/install.sh
@@ -0,0 +1,652 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# install.sh
+#	This script installs my basic setup for a debian laptop
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Choose a user account to use for this installation
+get_user() {
+	if [[ -z "${TARGET_USER-}" ]]; then
+		mapfile -t options < <(find /home/* -maxdepth 0 -printf "%f\\n" -type d)
+		# if there is only one option just use that user
+		if [ "${#options[@]}" -eq "1" ]; then
+			readonly TARGET_USER="${options[0]}"
+			echo "Using user account: ${TARGET_USER}"
+			return
+		fi
+
+		# iterate through the user options and print them
+		PS3='command -v user account should be used? '
+
+		select opt in "${options[@]}"; do
+			readonly TARGET_USER=$opt
+			break
+		done
+	fi
+}
+
+check_is_sudo() {
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root."
+		exit
+	fi
+}
+
+
+setup_sources_min() {
+	apt update || true
+	apt install -y \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		dirmngr \
+		gnupg2 \
+		lsb-release \
+		--no-install-recommends
+
+	# hack for latest git (don't judge)
+	cat <<-EOF > /etc/apt/sources.list.d/git-core.list
+	deb http://ppa.launchpad.net/git-core/ppa/ubuntu xenial main
+	deb-src http://ppa.launchpad.net/git-core/ppa/ubuntu xenial main
+	EOF
+
+	# iovisor/bcc-tools
+	cat <<-EOF > /etc/apt/sources.list.d/iovisor.list
+	deb https://repo.iovisor.org/apt/xenial xenial main
+	EOF
+
+	# add the git-core ppa gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
+
+	# add the iovisor/bcc-tools gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 648A4A16A23015EEF4A66B8E4052245BD4284CDD
+
+	# turn off translations, speed up apt update
+	mkdir -p /etc/apt/apt.conf.d
+	echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
+}
+
+# sets up apt sources
+# assumes you are going to use debian buster
+setup_sources() {
+	setup_sources_min;
+
+	cat <<-EOF > /etc/apt/sources.list
+	deb http://httpredir.debian.org/debian sid main contrib non-free
+	deb-src http://httpredir.debian.org/debian/ sid main contrib non-free
+	EOF
+
+	# yubico
+	cat <<-EOF > /etc/apt/sources.list.d/yubico.list
+	deb http://ppa.launchpad.net/yubico/stable/ubuntu xenial main
+	deb-src http://ppa.launchpad.net/yubico/stable/ubuntu xenial main
+	EOF
+
+	# Add the Cloud SDK distribution URI as a package source
+	cat <<-EOF > /etc/apt/sources.list.d/google-cloud-sdk.list
+	deb https://packages.cloud.google.com/apt cloud-sdk main
+	EOF
+
+	# Import the Google Cloud Platform public key
+	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+	# Add the Google Chrome distribution URI as a package source
+	cat <<-EOF > /etc/apt/sources.list.d/google-chrome.list
+	deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
+	EOF
+
+	# Import the Google Chrome public key
+	curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+	# add the yubico ppa gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 3653E21064B19D134466702E43D5C49532CBA1A9
+}
+
+base_min() {
+	apt update || true
+	apt -y upgrade
+
+	apt install -y \
+		adduser \
+		automake \
+		bash-completion \
+		bc \
+		bzip2 \
+		ca-certificates \
+		coreutils \
+		curl \
+		dnsutils \
+		file \
+		findutils \
+		gcc \
+		git \
+		gnupg \
+		gnupg2 \
+		grep \
+		gzip \
+		hostname \
+		indent \
+		iptables \
+		jq \
+		less \
+		libc6-dev \
+		locales \
+		lsof \
+		make \
+		mount \
+		net-tools \
+		policykit-1 \
+		silversearcher-ag \
+		ssh \
+		strace \
+		sudo \
+		tar \
+		tree \
+		tzdata \
+		unzip \
+		vim \
+		xz-utils \
+		zip \
+		--no-install-recommends
+
+	apt autoremove -y
+	apt autoclean -y
+	apt clean -y
+
+	install_scripts
+}
+
+# installs base packages
+# the utter bare minimal shit
+base() {
+	base_min;
+
+	apt update || true
+	apt -y upgrade
+
+	apt install -y \
+		apparmor \
+		bridge-utils \
+		cgroupfs-mount \
+		fwupd \
+		fwupdate \
+		gnupg-agent \
+		google-cloud-sdk \
+		iwd \
+		libapparmor-dev \
+		libimobiledevice6 \
+		libltdl-dev \
+		libpam-systemd \
+		libpcsclite-dev \
+		libseccomp-dev \
+		pcscd \
+		pinentry-curses \
+		scdaemon \
+		systemd \
+		--no-install-recommends
+
+	setup_sudo
+
+	apt autoremove -y
+	apt autoclean -y
+	apt clean -y
+}
+
+# install and configure dropbear
+install_dropbear() {
+	apt update || true
+	apt -y upgrade
+
+	apt install -y \
+		dropbear-initramfs \
+		--no-install-recommends
+
+	apt autoremove -y
+	apt autoclean -y
+	apt clean -y
+
+	# change the default port and settings
+	echo 'DROPBEAR_OPTIONS="-p 4748 -s -j -k -I 60"' >> /etc/dropbear-initramfs/config
+
+	# update the authorized keys
+	cp "/home/${TARGET_USER}/.ssh/authorized_keys" /etc/dropbear-initramfs/authorized_keys
+	sed -i 's/ssh-/no-port-forwarding,no-agent-forwarding,no-X11-forwarding,command="\/bin\/cryptroot-unlock" ssh-/g' /etc/dropbear-initramfs/authorized_keys
+
+	echo
+	echo "Updated config in /etc/dropbear-initramfs/config:"
+	cat /etc/dropbear-initramfs/config
+	echo
+
+	echo "Updated authorized_keys in /etc/dropbear-initramfs/authorized_keys:"
+	cat /etc/dropbear-initramfs/authorized_keys
+	echo
+
+	echo "Dropbear has been installed and configured."
+	echo
+	echo "You will now want to update your initramfs:"
+	printf "\\tupdate-initramfs -u\\n"
+}
+
+# setup sudo for a user
+# because fuck typing that shit all the time
+# just have a decent password
+# and lock your computer when you aren't using it
+# if they have your password they can sudo anyways
+# so its pointless
+# i know what the fuck im doing ;)
+setup_sudo() {
+	# add user to sudoers
+	adduser "$TARGET_USER" sudo
+
+	# add user to systemd groups
+	# then you wont need sudo to view logs and shit
+	gpasswd -a "$TARGET_USER" systemd-journal
+	gpasswd -a "$TARGET_USER" systemd-network
+
+	# create docker group
+	sudo groupadd docker
+	sudo gpasswd -a "$TARGET_USER" docker
+
+	# add go path to secure path
+	{ \
+		echo -e "Defaults	secure_path=\"/usr/local/go/bin:/home/${TARGET_USER}/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/share/bcc/tools:/home/${TARGET_USER}/.cargo/bin\""; \
+		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
+		echo -e "${TARGET_USER} ALL=(ALL) NOPASSWD:ALL"; \
+		echo -e "${TARGET_USER} ALL=NOPASSWD: /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
+	} >> /etc/sudoers
+
+	# setup downloads folder as tmpfs
+	# that way things are removed on reboot
+	# i like things clean but you may not want this
+	mkdir -p "/home/$TARGET_USER/Downloads"
+	echo -e "\\n# tmpfs for downloads\\ntmpfs\\t/home/${TARGET_USER}/Downloads\\ttmpfs\\tnodev,nosuid,size=50G\\t0\\t0" >> /etc/fstab
+}
+
+# install rust
+
+install_rust() {
+	curl https://sh.rustup.rs -sSf | sh
+
+	# Install rust-src for rust analyzer
+	rustup component add rust-src
+	# Install rust-analyzer
+	curl -sSL "https://github.com/rust-analyzer/rust-analyzer/releases/download/2020-04-20/rust-analyzer-linux" -o "${HOME}/.cargo/bin/rust-analyzer"
+	chmod +x "${HOME}/.cargo/bin/rust-analyzer"
+
+	# Install clippy
+	rustup component add clippy
+}
+
+# install/update golang from source
+install_golang() {
+	export GO_VERSION
+	GO_VERSION=$(curl -sSL "https://golang.org/VERSION?m=text")
+	export GO_SRC=/usr/local/go
+
+	# if we are passing the version
+	if [[ -n "$1" ]]; then
+		GO_VERSION=$1
+	fi
+
+	# purge old src
+	if [[ -d "$GO_SRC" ]]; then
+		sudo rm -rf "$GO_SRC"
+		sudo rm -rf "$GOPATH"
+	fi
+
+	GO_VERSION=${GO_VERSION#go}
+
+	# subshell
+	(
+	kernel=$(uname -s | tr '[:upper:]' '[:lower:]')
+	curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.${kernel}-amd64.tar.gz" | sudo tar -v -C /usr/local -xz
+	local user="$USER"
+	# rebuild stdlib for faster builds
+	sudo chown -R "${user}" /usr/local/go/pkg
+	CGO_ENABLED=0 go install -a -installsuffix cgo std
+	)
+
+	# get commandline tools
+	(
+	set -x
+	set +e
+	go get golang.org/x/lint/golint
+	go get golang.org/x/tools/cmd/cover
+	go get golang.org/x/tools/gopls
+	go get golang.org/x/review/git-codereview
+	go get golang.org/x/tools/cmd/goimports
+	go get golang.org/x/tools/cmd/gorename
+	go get golang.org/x/tools/cmd/guru
+
+	go get github.com/genuinetools/amicontained
+	go get github.com/genuinetools/apk-file
+	go get github.com/genuinetools/audit
+	go get github.com/genuinetools/bpfd
+	go get github.com/genuinetools/bpfps
+	go get github.com/genuinetools/certok
+	go get github.com/genuinetools/netns
+	go get github.com/genuinetools/pepper
+	go get github.com/genuinetools/reg
+	go get github.com/genuinetools/udict
+	go get github.com/genuinetools/weather
+
+	go get github.com/jessfraz/gmailfilters
+	go get github.com/jessfraz/junk/sembump
+	go get github.com/jessfraz/secping
+	go get github.com/jessfraz/ship
+	go get github.com/jessfraz/tdash
+
+	go get github.com/axw/gocov/gocov
+	go get honnef.co/go/tools/cmd/staticcheck
+
+	# Tools for vimgo.
+	go get github.com/jstemmer/gotags
+	go get github.com/nsf/gocode
+	go get github.com/rogpeppe/godef
+
+	aliases=( genuinetools/contained.af genuinetools/binctr genuinetools/img docker/docker moby/buildkit opencontainers/runc )
+	for project in "${aliases[@]}"; do
+		owner=$(dirname "$project")
+		repo=$(basename "$project")
+		if [[ -d "${HOME}/${repo}" ]]; then
+			rm -rf "${HOME:?}/${repo}"
+		fi
+
+		mkdir -p "${GOPATH}/src/github.com/${owner}"
+
+		if [[ ! -d "${GOPATH}/src/github.com/${project}" ]]; then
+			(
+			# clone the repo
+			cd "${GOPATH}/src/github.com/${owner}"
+			git clone "https://github.com/${project}.git"
+			# fix the remote path, since our gitconfig will make it git@
+			cd "${GOPATH}/src/github.com/${project}"
+			git remote set-url origin "https://github.com/${project}.git"
+			)
+		else
+			echo "found ${project} already in gopath"
+		fi
+
+		# make sure we create the right git remotes
+		if [[ "$owner" != "jessfraz" ]] && [[ "$owner" != "genuinetools" ]]; then
+			(
+			cd "${GOPATH}/src/github.com/${project}"
+			git remote set-url --push origin no_push
+			git remote add jessfraz "https://github.com/jessfraz/${repo}.git"
+			)
+		fi
+	done
+
+	# do special things for k8s GOPATH
+	mkdir -p "${GOPATH}/src/k8s.io"
+	kubes_repos=( community kubernetes release sig-release )
+	for krepo in "${kubes_repos[@]}"; do
+		git clone "https://github.com/kubernetes/${krepo}.git" "${GOPATH}/src/k8s.io/${krepo}"
+		cd "${GOPATH}/src/k8s.io/${krepo}"
+		git remote set-url --push origin no_push
+		git remote add jessfraz "https://github.com/jessfraz/${krepo}.git"
+	done
+	)
+
+	# symlink weather binary for motd
+	sudo ln -snf "${GOPATH}/bin/weather" /usr/local/bin/weather
+}
+
+# install graphics drivers
+install_graphics() {
+	local system=$1
+
+	if [[ -z "$system" ]]; then
+		echo "You need to specify whether it's intel, geforce or optimus"
+		exit 1
+	fi
+
+	local pkgs=( xorg xserver-xorg xserver-xorg-input-libinput xserver-xorg-input-synaptics )
+
+	case $system in
+		"intel")
+			pkgs+=( xserver-xorg-video-intel )
+			;;
+		"geforce")
+			pkgs+=( nvidia-driver )
+			;;
+		"optimus")
+			pkgs+=( nvidia-kernel-dkms bumblebee-nvidia primus )
+			;;
+		*)
+			echo "You need to specify whether it's intel, geforce or optimus"
+			exit 1
+			;;
+	esac
+
+	apt update || true
+	apt -y upgrade
+
+	apt install -y "${pkgs[@]}" --no-install-recommends
+}
+
+# install custom scripts/binaries
+install_scripts() {
+	# install speedtest
+	curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  > /usr/local/bin/speedtest
+	chmod +x /usr/local/bin/speedtest
+
+	# install icdiff
+	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
+	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
+	chmod +x /usr/local/bin/icdiff
+	chmod +x /usr/local/bin/git-icdiff
+
+	# install lolcat
+	curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
+	chmod +x /usr/local/bin/lolcat
+
+
+	local scripts=( have light )
+
+	for script in "${scripts[@]}"; do
+		curl -sSL "https://misc.j3ss.co/binaries/$script" > "/usr/local/bin/${script}"
+		chmod +x "/usr/local/bin/${script}"
+	done
+}
+
+# install stuff for i3 window manager
+install_wmapps() {
+	sudo apt update || true
+	sudo apt install -y \
+		bluez \
+		bluez-firmware \
+		feh \
+		google-chrome-stable \
+		i3 \
+		i3lock \
+		i3status \
+		pulseaudio \
+		pulseaudio-module-bluetooth \
+		pulsemixer \
+		rofi \
+		rxvt-unicode-256color \
+		scrot \
+		usbmuxd \
+		xclip \
+		xcompmgr \
+		--no-install-recommends
+
+	# start and enable pulseaudio
+	systemctl --user daemon-reload
+	systemctl --user enable pulseaudio.service
+	systemctl --user enable pulseaudio.socket
+	systemctl --user start pulseaudio.service
+
+	echo "Fonts file setup successfully now run:"
+	echo "	dpkg-reconfigure fontconfig-config"
+	echo "with settings: "
+	echo "	Autohinter, Automatic, No."
+	echo "Run: "
+	echo "	dpkg-reconfigure fontconfig"
+}
+
+get_dotfiles() {
+	# create subshell
+	(
+	cd "$HOME"
+
+	if [[ ! -d "${HOME}/dotfiles" ]]; then
+		# install dotfiles from repo
+		git clone git@github.com:jessfraz/dotfiles.git "${HOME}/dotfiles"
+	fi
+
+	cd "${HOME}/dotfiles"
+
+	# set the correct origin
+	git remote set-url origin git@github.com:jessfraz/dotfiles.git
+
+	# installs all the things
+	make
+
+	# enable dbus for the user session
+	# systemctl --user enable dbus.socket
+
+	sudo systemctl enable "i3lock@${TARGET_USER}"
+
+	cd "$HOME"
+	mkdir -p ~/Pictures/Screenshots
+	)
+
+	install_vim;
+}
+
+install_vim() {
+	# Install node, needed for coc.vim
+	curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+
+	# FROM: https://github.com/nodesource/distributions/blob/master/README.md
+	# Replace with the branch of Node.js or io.js you want to install: node_6.x,
+	# node_8.x, etc...
+	VERSION=node_14.x
+	# The below command will set this correctly, but if lsb_release isn't available, you can set it manually:
+	# - For Debian distributions: jessie, sid, etc...
+	# - For Ubuntu distributions: xenial, bionic, etc...
+	# - For Debian or Ubuntu derived distributions your best option is to use
+	# the codename corresponding to the upstream release your distribution is
+	# based off. This is an advanced scenario and unsupported if your
+	# distribution is not listed as supported per earlier in this README.
+	DISTRO="$(lsb_release -s -c)"
+	echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+	echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+
+	sudo apt update || true
+	sudo apt install -y \
+		nodejs \
+		--no-install-recommends
+
+	# create subshell
+	(
+	cd "$HOME"
+
+	# install .vim files
+	sudo rm -rf "${HOME}/.vim"
+	git clone --recursive git@github.com:jessfraz/.vim.git "${HOME}/.vim"
+	(
+	cd "${HOME}/.vim"
+	make install
+	)
+
+	# update alternatives to vim
+	sudo update-alternatives --install /usr/bin/vi vi "$(command -v vim)" 60
+	sudo update-alternatives --config vi
+	sudo update-alternatives --install /usr/bin/editor editor "$(command -v vim)" 60
+	sudo update-alternatives --config editor
+	)
+}
+
+install_tools() {
+	echo "Installing golang..."
+	echo
+	install_golang;
+
+	echo
+	echo "Installing rust..."
+	echo
+	install_rust;
+
+	echo
+	echo "Installing scripts..."
+	echo
+	sudo install.sh scripts;
+}
+
+usage() {
+	echo -e "install.sh\\n\\tThis script installs my basic setup for a debian laptop\\n"
+	echo "Usage:"
+	echo "  base                                - setup sources & install base pkgs"
+	echo "  basemin                             - setup sources & install base min pkgs"
+	echo "  graphics {intel, geforce, optimus}  - install graphics drivers"
+	echo "  wm                                  - install window manager/desktop pkgs"
+	echo "  dotfiles                            - get dotfiles"
+	echo "  vim                                 - install vim specific dotfiles"
+	echo "  golang                              - install golang and packages"
+	echo "  rust                                - install rust"
+	echo "  scripts                             - install scripts"
+	echo "  tools                               - install golang, rust, and scripts"
+	echo "  dropbear                            - install and configure dropbear initramfs"
+}
+
+main() {
+	local cmd=$1
+
+	if [[ -z "$cmd" ]]; then
+		usage
+		exit 1
+	fi
+
+	if [[ $cmd == "base" ]]; then
+		check_is_sudo
+		get_user
+
+		# setup /etc/apt/sources.list
+		setup_sources
+
+		base
+	elif [[ $cmd == "basemin" ]]; then
+		check_is_sudo
+		get_user
+
+		# setup /etc/apt/sources.list
+		setup_sources_min
+
+		base_min
+	elif [[ $cmd == "graphics" ]]; then
+		check_is_sudo
+
+		install_graphics "$2"
+	elif [[ $cmd == "wm" ]]; then
+		install_wmapps
+	elif [[ $cmd == "dotfiles" ]]; then
+		get_user
+		get_dotfiles
+	elif [[ $cmd == "vim" ]]; then
+		install_vim
+	elif [[ $cmd == "rust" ]]; then
+		install_rust
+	elif [[ $cmd == "golang" ]]; then
+		install_golang "$2"
+	elif [[ $cmd == "scripts" ]]; then
+		install_scripts
+	elif [[ $cmd == "tools" ]]; then
+		install_tools
+	elif [[ $cmd == "dropbear" ]]; then
+		check_is_sudo
+
+		get_user
+
+		install_dropbear
+	else
+		usage
+	fi
+}
+
+main "$@"
